e43803747406f1d4cb1670436f1f970ce2a7814f
Lara-br <l.a.van.den.broek@student.rug.nl>
2025-09-15 10:42:45 +0200

    knn add code to functions

--- Exercises/KNN and KMeans/knn.py
+++ Exercises/KNN and KMeans/knn.py
@@ -5,17 +5,47 @@ from helper import *
 
 class KNN(ModelABC):
     def __init__(self, k: int = 3) -> None:
-        pass
+        """
+        Initializes the KNN classifier with the specified number of neighbors.
+        Args:
+            k (int, optional): The number of nearest neighbors to use for classification. Defaults to 3.
+        """
+        self.k: int = k
+        self._parameters: dict = {}
 
     def fit(self, observations: np.ndarray, labels: np.ndarray) -> None:
-        pass
+        """
+        Fits the KNN model to the provided training data.
+        Args:
+            observations (np.ndarray): The feature matrix of training data, where each row represents an observation.
+            labels (np.ndarray): The corresponding labels for each observation.
+        Returns:
+            None
+        """
+        self._parameters: dict = {"observations": observations, "labels": labels}
 
     def predict(self, observations: np.ndarray) -> np.ndarray:
-        predictions = [self._predict_single(x) for x in observations]
+         """
+        Predicts the class labels for the given observations using the trained k-NN model.
+        Args:
+            observations (np.ndarray): A 2D numpy array where each row represents an observation to classify.
+        Returns:
+            np.ndarray: An array of predicted class labels for each observation.
+        """
+        predictions: list = [self._predict_single(x) for x in observations]
         return np.array(predictions)
 
-    def _predict_single(self) -> None:
-        pass
+    def _predict_single(self, x: np.ndarray) -> None:
+        """
+        Predicts the label for a single input sample using the k-nearest neighbors algorithm.
+        Args:
+            x (np.ndarray): The input sample for which to predict the label. Should be a 1D numpy array representing feature values.
+        Returns:
+            int: The predicted label for the input sample, determined by majority vote among the k nearest neighbors.
+        """
+        distance: int = np.linalg.normm(self._parameters["observations"] - x, axis=1)
+        nn_indices = np.
+        
 
 
 data, labels, apples, oranges = generate_true_data(50)
